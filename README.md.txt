47abeb9922588bbe1fbf0da3bd646ea930c1eaf4
Juarez Geraldo Silva Jr <juarez.junior@grupomult.com.br>
2022-09-05 17:57:41 -0300

    Commit inicial

---
+++ README.md.txt
@@ -0,0 +1,8 @@
+# infnet-bl5-smpaa-tp1
+## Instituto Infnet
+### Análise e Desenvolvimento de Sistemas - Segurança, Monetização e Publicação de Aplicativos Android
+### Análise e Desenvolvimento de Sistemas - Desenvolvimento Kotlin
+### Trabalho Prático 3
+#### Professor: Armênio Torres Santiago Cardoso
+#### Aluno: Juarez Geraldo da Silva junior
+Trabalho Prático 3 das disciplinas de Segurança, Monetização e Publicação de Aplicativos Android e Desenvolvimento Kotlin para demonstrar as competências trabalhadas nas etapas 5 e 6.
---
+++ app/src/main/java/com/infnet/juarez/avaliacaolimpeza/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.infnet.juarez.avaliacaolimpeza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+
+class MainActivity : AppCompatActivity() {
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult){
+        if (result.resultCode == RESULT_OK){
+
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build())
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+}
